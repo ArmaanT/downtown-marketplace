4d9598bc97c8bb4358a9b5c22160f09d643009be
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-04-21 17:11:15 -0400

    Fix flake8 errors

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from main.models import Downtown, Ticket, User
 
+
 admin.site.register(Downtown)
 admin.site.register(Ticket)
 admin.site.register(User)
--- main/models.py
+++ main/models.py
@@ -1,6 +1,6 @@
-from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
@@ -22,4 +22,4 @@ class Ticket(models.Model):
     downtown = models.ForeignKey(Downtown, on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return str(self.seller) + ": " + str(self.downtown)
+        return str(self.seller) + ': ' + str(self.downtown)
