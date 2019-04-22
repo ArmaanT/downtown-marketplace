9701f2af6b9400289fd3530fb28735e98de22c15
Peyton Walters <pwpon500@gmail.com>
2019-04-21 22:12:18 -0400

    Implemented basic authentication

--- marketplace/settings/base.py
+++ marketplace/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Custom User Model
 
 AUTH_USER_MODEL = 'main.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
