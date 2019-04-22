4664d00cf0b12ac24d8152c01e83518c80654452
Peyton Walters <pwpon500@gmail.com>
2019-04-21 22:27:04 -0400

    Used pipenv to install widget_tweaks

--- marketplace/settings/base.py
+++ marketplace/settings/base.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig',
     'widget_tweaks',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
