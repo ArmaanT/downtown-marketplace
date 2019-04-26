6020208eb73fe694202ad02239d5da2253311c18
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-04-26 17:08:37 -0400

    Add logout url

--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import include, path
 
 from main.views import HomeView
@@ -5,5 +6,6 @@ from main.views import HomeView
 
 urlpatterns = [
     path('', HomeView.as_view()),
+    path('accounts/logout', LogoutView.as_view(next_page='/'), name='logout'),
     path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
