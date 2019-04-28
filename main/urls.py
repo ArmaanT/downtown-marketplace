22f0c40690b5baa28d14cda9140dcfbd51811d67
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-04-28 14:56:38 -0400

    Fix logout redirect

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,5 @@ from main.views import HomeView
 urlpatterns = [
     path('', HomeView.as_view()),
     path('accounts/login', LoginView.as_view(template_name='main/login.html'), name='login'),
-    path('accounts/logout', LogoutView.as_view(), name='logout'),
+    path('accounts/logout', LogoutView.as_view(next_page='/'), name='logout'),
 ]
