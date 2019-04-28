eb68088dac0b0ab7f3d007c9888c9b5194dd819a
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-04-28 15:42:33 -0400

    Add signup form

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from main.views import HomeView
+from main.views import HomeView, SignupView
 
 
 urlpatterns = [
     path('', HomeView.as_view()),
     path('accounts/login', LoginView.as_view(template_name='main/login.html'), name='login'),
     path('accounts/logout', LogoutView.as_view(next_page='/'), name='logout'),
+    path('accounts/signup', SignupView.as_view(), name='signup')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
 
+from main.forms import SignupForm
 from main.models import Downtown
 
 
@@ -10,3 +13,13 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['downtowns'] = Downtown.objects.all()
         return context
+
+
+class SignupView(FormView):
+    template_name = 'main/signup.html'
+    form_class = SignupForm
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
