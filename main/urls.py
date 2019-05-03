bdf065188371427df43da953743759c5cf5f4d02
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-05-03 14:51:27 -0400

    Uniform URL slashes

--- main/urls.py
+++ main/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('', HomeView.as_view()),
     path('accounts/follow/', FollowView.as_view()),
     path('accounts/unfollow/', UnfollowView.as_view()),
-    path('accounts/login', LoginView.as_view(template_name='main/login.html'), name='login'),
-    path('accounts/logout', LogoutView.as_view(next_page='/'), name='logout'),
-    path('accounts/signup', SignupView.as_view(), name='signup'),
-    path('accounts/profile/<slug:username>', ProfileView.as_view(), name='profile')
+    path('accounts/login/', LoginView.as_view(template_name='main/login.html'), name='login'),
+    path('accounts/logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('accounts/signup/', SignupView.as_view(), name='signup'),
+    path('accounts/profile/<slug:username>/', ProfileView.as_view(), name='profile')
 ]
