161f74912713dd02b25b6b57d0d677a486072fc1
Peyton Walters <pwpon500@gmail.com>
2019-05-05 23:17:46 -0400

    Fix downtown page

--- main/urls.py
+++ main/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('accounts/logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('accounts/signup/', SignupView.as_view(), name='signup'),
     path('accounts/profile/<slug:username>/', ProfileView.as_view(), name='profile'),
-    path('downtowns/<int:dtname>', DowntownView.as_view()),
+    path('downtowns/<int:dtname>', DowntownView.as_view(), name='downtowns'),
     path('sell/', SellView.as_view(), name='sell'),
 ]
